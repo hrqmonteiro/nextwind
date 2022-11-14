2e9db5a6a4438a0800eccacfa246ea2b10863e21
hrq <hrqmonteiro@protonmail.com>
2022-11-13 22:15:40 -0300

    :sparkles: setup prettier, jest, tailwind, configs, folders

---
+++ src/__mocks__/fileMock.ts
@@ -0,0 +1,10 @@
+// Read more at "Handling stylesheets and image imports" on https://nextjs.org/docs/testing
+
+const fileMock = {
+  src: '/img.jpg',
+  height: 24,
+  width: 24,
+  blurDataURL: 'data:image/png;base64,imagedata'
+}
+
+export default fileMock
---
+++ src/__mocks__/styleMock.ts
@@ -0,0 +1,7 @@
+// Mock CSS files.
+// If you're using CSS modules, this file can be deleted.
+// Read more at "Handling stylesheets and image imports" on https://nextjs.org/docs/testing
+
+const styleMock = {}
+
+export default styleMock
